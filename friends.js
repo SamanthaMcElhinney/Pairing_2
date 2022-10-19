2d28f18618ae049c457529f63852daade8567aba
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-10-19 11:38:31 -0700

    Add array method to new bar

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["Samantha", "Amy", "Dave", "Kiara", "Kelli"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var placesToVisit = ["Barcelona", "Turkey", "Costa Rica", "Iceland", "Mexico"];
+placesToVisit.pop();
+console.log(placesToVisit)
